19f1147905b1211c730f3791d2b5016a5d9c125e
Arghyadeep-code <arghyadeep.exam@gmail.com>
2026-06-28 19:30:18 +0530

    main code

---
+++ calc.py
@@ -0,0 +1,6 @@
+n=int(input("which number's multiplication table do you want to generate?"))
+m=int(input("till how much do you want to generate?"))
+i=0
+while i<=m:
+    print(n," x ",i," = ",n*i)
+    i+=1
